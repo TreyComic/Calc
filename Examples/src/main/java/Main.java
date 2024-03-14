8020d75c5cddb9543105ebcda02c97be31dfff8d
TreyComic <trey.greene1153@gmail.com>
2024-03-14 10:41:36 -0400

    :

--- Examples/src/main/java/Main.java
+++ Examples/src/main/java/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         //a constant variable
-        String productName = "milk";
-        productName = "bread";
-        float price = 20.0f;
-        boolean isonsale = true;
-        long inventory = 200L;
-        final String expirationDate = "03/21/2024";
+        //String productName = "milk";
+        //productName = "bread";
+        //float price = 20.0f;
+        //boolean isonsale = true;
+        //long inventory = 200L;
+        //final String expirationDate = "03/21/2024";
         //expiredate = "10/31/2024"
 
-        double myPI = 3.14;
+        //double myPI = 3.14;
 
 
 
--- Examples/src/main/java/Math.java
+++ Examples/src/main/java/Math.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
 public class Math {
     public static void main(String[] args){
 
+        //Create a scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //What is the item name?
+        System.out.println("Enter item name: ");
+        String Name = scanner.nextLine();
+
+        //prompt for our Price
+        System.out.println("Enter item price: ");
+        Float price = scanner.nextFloat();
+
+
+
+        //how many items?
+        System.out.println("Enter item quantity: ");
+        Float qty = scanner.nextFloat();
+
+        //How far
+        System.out.println("Enter distance for delivery (Miles): ");
+        Float miles = scanner.nextFloat();
+
+        scanner.close();
+
         //calculate fees
-        float price = 5.0f;
-        float qty = 20.0f;
-        float miles = 10.0f;
-        float fee = 0.0f;
+        //float price = 5.25f;
+        //float qty = 9.0f;
+        //float miles = 10.0f;
+        Float fee = 0.0f;
         if(miles <= 4.0f){
             fee += 2.0f;
         }
